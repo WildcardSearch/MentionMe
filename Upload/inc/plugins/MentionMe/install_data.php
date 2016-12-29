6978c1a3e6eea7440bce72612705b36d9716f2dc
Mark Vincent <admin@rantcentralforums.com>
2016-12-29 16:51:50 -0600

    Fixes #107
    ...where a div was left open in the popup HTML.

--- Upload/inc/plugins/MentionMe/install_data.php
+++ Upload/inc/plugins/MentionMe/install_data.php
@@ -116,7 +116,7 @@ EOF
 	<div id="mentionme_popup_input_container">
 		<input id="mentionme_popup_input" type="text" autocomplete="off" />
 	</div>
-	<div id="mentionme_popup_body">
+	<div id="mentionme_popup_body"></div>
 </div>
 EOF
 		),
