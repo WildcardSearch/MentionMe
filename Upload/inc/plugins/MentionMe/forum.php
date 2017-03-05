08ef7d51acde5533672e90832c9aaf0cb95822c3
Sofartin <hrubymar10@gmail.com>
2017-03-05 13:32:49 +0100

    Provide Full Forum URL
    I think that it is better to provide full forum url.
    
    In our case this plugin didn't work because of this...

--- Upload/inc/plugins/MentionMe/forum.php
+++ Upload/inc/plugins/MentionMe/forum.php
@@ -247,7 +247,7 @@ function mentionBuild($user)
 		// set up the user name link so that it displays correctly for the display group of the user
 		$username = format_name($username, $user['usergroup'], $user['displaygroup']);
 	}
-	$url = get_profile_link($user['uid']);
+	$url = $mybb->settings['bburl'] . "/" . get_profile_link($user['uid']);
 
 	// the HTML id property is used to store the uid of the mentioned user for MyAlerts (if installed)
 	return <<<EOF
