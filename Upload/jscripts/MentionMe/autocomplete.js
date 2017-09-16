5b7a1d5a7cbd695a9c9e4455760dd2d1120af2d8
Mark Vincent <admin@rantcentralforums.com>
2017-09-16 17:18:56 -0500

    Fixes #157
    ...where a previous patch is no longer necessary and was replaced.

--- Upload/jscripts/MentionMe/autocomplete.js
+++ Upload/jscripts/MentionMe/autocomplete.js
@@ -45,56 +45,6 @@ var MentionMe = (function($, m) {
 		},
 
 	/**
-	 * for older browsers
-	 * From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-	 */
-	getObjectKeys = (typeof Object.keys === "function") ?
-		Object.keys :
-		(function() {
-		"use strict";
-
-		var hasOwnProperty = Object.prototype.hasOwnProperty,
-			hasDontEnumBug = !({ toString: null }).propertyIsEnumerable("toString"),
-			dontEnums = [
-				"toString",
-				"toLocaleString",
-				"valueOf",
-				"hasOwnProperty",
-				"isPrototypeOf",
-				"propertyIsEnumerable",
-				"constructor"
-			],
-			dontEnumsLength = dontEnums.length;
-
-		return function(obj) {
-			if (typeof obj !== "object" &&
-				(typeof obj !== "function" ||
-				obj === null)) {
-				throw new TypeError("Object.keys called on non-object");
-			}
-
-			var result = [],
-				prop,
-				i;
-
-			for (prop in obj) {
-				if (hasOwnProperty.call(obj, prop)) {
-					result.push(prop);
-				}
-			}
-
-			if (hasDontEnumBug) {
-				for (i = 0; i < dontEnumsLength; i++) {
-					if (hasOwnProperty.call(obj, dontEnums[i])) {
-						result.push(dontEnums[i]);
-					}
-				}
-			}
-			return result;
-		};
-	}()),
-
-	/**
 	 * the popup object
 	 */
 	popup = (function() {
@@ -1536,7 +1486,7 @@ var MentionMe = (function($, m) {
 		 * @return void
 		 */
 		function init() {
-			var key = getObjectKeys(CKEDITOR.instances)[0];
+			var key = $.map(CKEDITOR.instances, function(i, k) { return k })[0];
 
 			if (typeof CKEDITOR.instances[key] !== "object") {
 				return false;
