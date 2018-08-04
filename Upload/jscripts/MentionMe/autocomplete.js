f4f100db832912f20b50ace965d67efef409ecee
Mark Vincent <admin@rantcentralforums.com>
2018-08-04 15:06:26 -0500

    Formatting

--- Upload/jscripts/MentionMe/autocomplete.js
+++ Upload/jscripts/MentionMe/autocomplete.js
@@ -299,11 +299,13 @@ var MentionMe = (function($, m) {
 				avatar = "";
 				if (options.showAvatars) {
 					avatarPath = data[user]["avatar"];
-					if (typeof avatarPath == 'undefined') {
+
+					if (typeof avatarPath == "undefined") {
 						avatarPath = options.defaultAvatar;
 					} else if (avatarPath.length == 0) {
 						avatarPath = options.defaultAvatar;
 					}
+
 					avatar = $("<img/>", {
 						"class": "mention_user_avatar",
 						src: avatarPath,
